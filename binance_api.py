72dfc94d4791f66660e043f85ba30b6719126be5
zachlegros <legroszach3921@gmail.com>
2021-04-18 14:23:48 -0400

    Implemented get_candles in binance_api to fetch klines with symbol and timeframe

---
+++ binance_api.py
@@ -0,0 +1,16 @@
+from credentials import api_key, api_secret
+from binance.client import Client
+import pandas as pd
+import numpy as np
+
+client = Client(api_key, api_secret)
+
+
+def get_candles(symbol, interval):
+    data = client.get_klines(symbol=symbol, interval=interval)
+    array = np.array(data)
+
+    df = pd.DataFrame(array[:, 0:5], columns={
+                      'T': 'time', 'O': 'open', 'H': 'high', 'L': 'low', 'C': 'close'}).astype(float)
+
+    return df
